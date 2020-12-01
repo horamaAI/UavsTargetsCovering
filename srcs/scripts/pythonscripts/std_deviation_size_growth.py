b89927e75bfd617d831e76ef01f2e057de79ddca
Dahom1er <mahoro.ntwari@gmail.com>
2020-12-01 23:57:54 +0100

    Approach's runtime growth analysis

---
+++ srcs/scripts/pythonscripts/std_deviation_size_growth.py
@@ -0,0 +1,28 @@
+
+import numpy as np
+
+targets=["set1_1000_1000_map", "set2_1500_1500_map", "set3_2000_2000_map", "set4_2500_2500_map", "set5_3000_3000_map",
+"set6_3500_3500_map"]
+
+alldata=[[[] for j in range(20)] for i in targets]
+
+k=0
+for i in targets:
+	for j in range(20):
+		with open("../outs/"+i+"/"+str((j+1)*50)+"_grounds_coords.csv") as f:
+			lines=f.readlines()
+			for x in lines:
+				row=x.rstrip('\n').split(',')
+				alldata[k][j].append([float(row[0]), float(row[1])])
+	k+=1
+
+avrgstds=[[] for i in targets]
+
+for i in range(len(alldata)):
+	for j in range(len(alldata[i])):
+		avrgstds[i].append(np.std(alldata[i][j]))
+	#print("==============================\n")
+
+for row in avrgstds:
+	print(np.std(row))
+	print("==============================\n")
