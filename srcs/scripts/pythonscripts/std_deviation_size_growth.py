f8a94b4113202e82c7556ebbff2de1fbd6059ac1
Dahom1er <mahoro.ntwari@gmail.com>
2021-02-07 21:28:26 +0100

    Still trying to fix conflicts

--- srcs/scripts/pythonscripts/std_deviation_size_growth.py
+++ srcs/scripts/pythonscripts/std_deviation_size_growth.py
@@ -9,9 +9,6 @@ import numpy as np
 targets=["set1_1000_1000_map", "set2_1500_1500_map", "set3_2000_2000_map", "set4_2500_2500_map", "set5_3000_3000_map",
 "set6_3500_3500_map"]
 
-sizexmap=[[0. for i in range(20)] for j in range(6)]
-sizeymap=[[0. for i in range(20)] for j in range(6)]
-
 alldata=[[[] for j in range(20)] for i in targets]
 
 k=0
@@ -19,40 +16,16 @@ for i in targets:
 	for j in range(20):
 		with open("../outs/"+i+"/"+str((j+1)*50)+"_grounds_coords.csv") as f:
 			lines=f.readlines()
-			maxx=0.
-			maxy=0.
 			for x in lines:
 				row=x.rstrip('\n').split(',')
 				alldata[k][j].append([float(row[0]), float(row[1])])
-				if float(row[0]) > maxx:
-					maxx=float(row[0])
-				if float(row[1]) > maxy:
-					maxy=float(row[1])
-			sizexmap[k][j]=maxx
-			sizeymap[k][j]=maxy
 	k+=1
-
-print("with sizes sizexmap == ", len(sizexmap), " VS sizeymap == ", len(sizeymap) )
-for alist in sizexmap:
-	print(alist)
-print("=======================")
-print("=======================")
-for alist in sizeymap:
-	print(alist)
-print("=======================")
-print("=======================")
-
-print("Averages of contents of sizexmap and sizeymap")
-for i in range(len(sizexmap)):
-	print((sum(sizexmap[i])/len(sizexmap[i])), " * ", (sum(sizeymap[i])/len(sizeymap[i])))
 
 avrgstds=[[] for i in targets]
 
 for i in range(len(alldata)):
-	print(i," starts :")
 	for j in range(len(alldata[i])):
 		avrgstds[i].append(np.std(alldata[i][j]))
-	print(np.percentile(avrgstds[i], [0, 25, 50, 75, 100]))
 
 #for row in avrgstds:
 #	print(len(row))
